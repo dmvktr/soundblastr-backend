396ca64a1efd5d1460f8722b82fde95db4b73070
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-21 14:39:14 +0200

    [add] dummy Band data for testing

--- src/main/java/com/codecool/soundblastr/SoundblastrBackendApplication.java
+++ src/main/java/com/codecool/soundblastr/SoundblastrBackendApplication.java
@@ -1,13 +1,38 @@
 package com.codecool.soundblastr;
 
+import com.codecool.soundblastr.entity.Band;
+import com.codecool.soundblastr.entity.Genre;
+import com.codecool.soundblastr.repository.BandRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SoundblastrBackendApplication {
 
+    @Autowired
+    private BandRepository bandRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(SoundblastrBackendApplication.class, args);
+    }
+
+    @Bean
+    @Profile("prod")
+    public CommandLineRunner init() {
+        return args -> {
+            final Band muse = Band.builder()
+                    .name("Muse")
+                    .genre(Genre.INDIE)
+                    .genre(Genre.POP)
+                    .build();
+
+            bandRepository.save(muse);
+
+        };
     }
 
 }

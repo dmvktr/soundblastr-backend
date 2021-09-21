0789b1603c375ca75661a61478cefcf16e798d37
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-21 16:06:31 +0200

    [add] addEvent to Band

--- src/main/java/com/codecool/soundblastr/entity/Band.java
+++ src/main/java/com/codecool/soundblastr/entity/Band.java
@@ -3,19 +3,35 @@ package com.codecool.soundblastr.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Builder
 public class Band {
 
     @Id
-    @GeneratedValue
+    @SequenceGenerator(
+            name = "band_sequence",
+            sequenceName = "band_sequence",
+            allocationSize = 1,
+            initialValue = 100
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "band_sequence"
+    )
+    @Column(
+            name="id",
+            updatable = false
+    )
     private Long id;
 
     @Column(
@@ -29,5 +45,17 @@ public class Band {
     @Singular
     @Enumerated(EnumType.STRING)
     private Set<Genre> genres;
+
+    @Singular
+    @OneToMany(mappedBy = "band", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    List<Event> events;
+
+    public void addEvent(Event event) {
+        if (events == null) {
+            events = new ArrayList<>();
+        }
+        events.add(event);
+    }
+
 
 }

47eec2d4250a2c95ae247d9a20df05daa3f283eb
Viktor Dome <v.dome1991@gmail.com>
2021-09-21 14:00:31 +0200

    [update] basic contextloads test

--- src/test/java/com/codecool/soundblastr/SoundblastrBackendApplicationTests.java
+++ src/test/java/com/codecool/soundblastr/SoundblastrBackendApplicationTests.java
@@ -1,13 +1,17 @@
 package com.codecool.soundblastr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class SoundblastrBackendApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }

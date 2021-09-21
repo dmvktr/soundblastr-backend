32e1b6c21d9344a596ead0c63cd1a81426621ec3
Viktor Dome <v.dome1991@gmail.com>
2021-09-21 13:47:46 +0200

    first commit

---
+++ src/test/java/com/codecool/soundblastr/SoundblastrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.soundblastr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundblastrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

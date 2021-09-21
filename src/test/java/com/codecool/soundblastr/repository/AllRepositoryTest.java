a9efcb67f97fed9ab379c84119aaf5640431d5d5
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-21 15:00:55 +0200

    [add] first two Band repo test

---
+++ src/test/java/com/codecool/soundblastr/repository/AllRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.codecool.soundblastr.repository;
+
+import com.codecool.soundblastr.entity.Band;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
+class AllRepositoryTest {
+
+    @Autowired
+    BandRepository bandRepository;
+
+    @Test
+    public void bandRepository_bandIsPersisted_whenBandAddedWithValidDetails() {
+        Band testBand = Band.builder().name("Test").build();
+        bandRepository.save(testBand);
+
+        List<Band> bandList = bandRepository.findAll();
+
+        assertThat(bandList).hasSize(1).anyMatch(band -> band.getName().equals("Test"));
+    }
+
+    @Test
+    public void bandRepository_exceptionThrown_whenBandAddedWithoutRequiredFields() {
+        Band testBand = Band.builder().build();
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            bandRepository.save(testBand);
+        });
+    }
+
+}

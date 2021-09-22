6d74cbf11a320058cd44a6737fcd33a2a69e1f1c
viktor <v.dome1991@gmail.com>
2021-09-22 11:46:50 +0200

    [add] AddressRepository

---
+++ src/main/java/com/codecool/soundblastr/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.soundblastr.repository;
+
+import org.apache.tomcat.jni.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}

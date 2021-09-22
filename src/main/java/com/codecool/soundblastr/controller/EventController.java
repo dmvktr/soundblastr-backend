d4c05809fe08511adee1a735b0249689e08d6966
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-22 17:17:41 +0200

    [add] and fix add Event POST API endpoint

--- src/main/java/com/codecool/soundblastr/controller/EventController.java
+++ src/main/java/com/codecool/soundblastr/controller/EventController.java
@@ -1,23 +1,48 @@
 package com.codecool.soundblastr.controller;
 
+import com.codecool.soundblastr.entity.Band;
 import com.codecool.soundblastr.entity.Event;
+import com.codecool.soundblastr.entity.EventRequest;
+import com.codecool.soundblastr.entity.Venue;
+import com.codecool.soundblastr.repository.BandRepository;
 import com.codecool.soundblastr.repository.EventRepository;
+import com.codecool.soundblastr.repository.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/event")
 public class EventController {
 
     private EventRepository eventRepository;
+    private BandRepository bandRepository;
+    private VenueRepository venueRepository;
 
     @Autowired
-    public EventController(EventRepository eventRepository) {
+    public EventController(EventRepository eventRepository, BandRepository bandRepository, VenueRepository venueRepository) {
         this.eventRepository = eventRepository;
+        this.bandRepository = bandRepository;
+        this.venueRepository = venueRepository;
+    }
+
+    @PostMapping("")
+    public Event addEvent(@RequestBody EventRequest eventRequest) {
+        Band band = bandRepository.getById(eventRequest.getBandId());
+        Venue venue = venueRepository.getById(eventRequest.getVenueId());
+        Event newEvent = Event.builder()
+                .title(eventRequest.getTitle())
+                .price(eventRequest.getPrice())
+                .date(eventRequest.getDate())
+                .band(band)
+                .venue(venue)
+                .build();
+        eventRepository.saveAndFlush(newEvent);
+        Event newEventDB = eventRepository.findById(3L).orElse(null);
+        System.out.println("Roky!: " + newEventDB);
+        return newEventDB;
     }
 
     @GetMapping("/all")
@@ -25,8 +50,8 @@ public class EventController {
         return eventRepository.findAll();
     }
 
-//    @GetMapping("/{eventId}")
-//    public List<Event> getAllEvents() {
-//        return eventRepository.findAll();
-//    }
+    @GetMapping("/{eventId}")
+    public Event getEvent(@PathVariable Long eventId) {
+        return eventRepository.findById(eventId).orElse(null);
+    }
 }

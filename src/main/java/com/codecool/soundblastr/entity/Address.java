be6f075858d30e159d91ef638062ca14e9147cb2
viktor <v.dome1991@gmail.com>
2021-09-22 11:46:35 +0200

    [add] Address entity

---
+++ src/main/java/com/codecool/soundblastr/entity/Address.java
@@ -0,0 +1,54 @@
+package com.codecool.soundblastr.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    @Id
+    @SequenceGenerator(
+        name = "address_sequence",
+        sequenceName = "address_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = SEQUENCE,
+        generator = "address_sequence"
+    )
+    @Column(
+        name="id",
+        updatable = false
+    )
+    private Long id;
+    @Column(
+        name="street",
+        nullable = false
+    )
+    private String street;
+    @Column(
+        name="houseNr",
+        nullable = false
+    )
+    private String houseNr;
+    @Column(
+        name="city",
+        nullable = false
+    )
+    private String city;
+    @Column(
+        name="zipcode",
+        nullable = false
+    )
+    private int zipcode;
+
+    @OneToOne(mappedBy = "address")
+    private Venue venue;
+}

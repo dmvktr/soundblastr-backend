54cd242fbc9bf812e32f597cc5531ff3882c86b1
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-23 13:06:08 +0200

    [remove] unnecessary method signature from VenueRepository

--- src/main/java/com/codecool/soundblastr/repository/VenueRepository.java
+++ src/main/java/com/codecool/soundblastr/repository/VenueRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VenueRepository extends JpaRepository<Venue, Long> {
-    Venue getVenueById(long venueId);
 }

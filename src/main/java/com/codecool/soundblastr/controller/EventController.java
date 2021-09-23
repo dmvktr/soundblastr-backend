3ec8098d5997cfed077548200071a7297bb818f0
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-23 11:17:51 +0200

    [update] add EventController routes

--- src/main/java/com/codecool/soundblastr/controller/EventController.java
+++ src/main/java/com/codecool/soundblastr/controller/EventController.java
@@ -1,9 +1,6 @@
 package com.codecool.soundblastr.controller;
 
-import com.codecool.soundblastr.entity.Band;
-import com.codecool.soundblastr.entity.Event;
-import com.codecool.soundblastr.entity.EventRequest;
-import com.codecool.soundblastr.entity.Venue;
+import com.codecool.soundblastr.entity.*;
 import com.codecool.soundblastr.repository.BandRepository;
 import com.codecool.soundblastr.repository.EventRepository;
 import com.codecool.soundblastr.repository.VenueRepository;
@@ -34,12 +31,18 @@ public class EventController {
         Venue venue = venueRepository.getById(eventRequest.getVenueId());
         Event newEvent = Event.builder()
                 .title(eventRequest.getTitle())
-                .price(eventRequest.getPrice())
+                .imageUrl(eventRequest.getImageUrl())
                 .date(eventRequest.getDate())
+                .ticketPrice(eventRequest.getTicketPrice())
                 .band(band)
                 .venue(venue)
                 .build();
         return eventRepository.save(newEvent);
+    }
+
+    @GetMapping("/{eventId}")
+    public Event getEvent(@PathVariable Long eventId) {
+        return eventRepository.findById(eventId).orElse(null);
     }
 
     @GetMapping("/all")
@@ -47,8 +50,34 @@ public class EventController {
         return eventRepository.findAll();
     }
 
-    @GetMapping("/{eventId}")
-    public Event getEvent(@PathVariable Long eventId) {
-        return eventRepository.findById(eventId).orElse(null);
+    @GetMapping("/band/{bandId}")
+    public List<Event> getEventsForBand(@PathVariable Long bandId) {
+        return eventRepository.findEventsByBandId(bandId);
     }
+
+    @GetMapping("/venue/{venueId}")
+    public List<Event> getEventsForVenue(@PathVariable Long venueId) {
+        return eventRepository.findEventsByVenueId(venueId);
+    }
+
+    @PutMapping("/{eventId}")
+    public Event updateEvent(@PathVariable Long eventId, @RequestBody EventRequest eventRequest) {
+        Band band = bandRepository.getById(eventRequest.getBandId());
+        Venue venue = venueRepository.getById(eventRequest.getVenueId());
+        Event eventToUpdate = eventRepository.getById(eventId);
+        eventToUpdate.setTitle(eventRequest.getTitle());
+        eventToUpdate.setImageUrl(eventRequest.getImageUrl());
+        eventToUpdate.setDate(eventRequest.getDate());
+        eventToUpdate.setTicketPrice(eventRequest.getTicketPrice());
+        eventToUpdate.setBand(band);
+        eventToUpdate.setVenue(venue);
+        return eventRepository.save(eventToUpdate);
+    }
+
+    @DeleteMapping("/{eventId}")
+    public JsonMessage deleteEvent(@PathVariable Long eventId) {
+        eventRepository.deleteById(eventId);
+        return new JsonMessage(Status.OK, "Successfully deleted event #" +  eventId + ".");
+    }
+
 }

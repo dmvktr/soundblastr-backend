ff2157fe93d5267abfbb0ca71209aaa4ee78499b
Viktor Dome <v.dome1991@gmail.com>
2021-09-30 21:02:11 +0200

    [add] global controller exception handler

---
+++ src/main/java/com/codecool/soundblastr/controller/ControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.codecool.soundblastr.controller;
+
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataAccessResourceFailureException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> resourceNotFound() {
+        return ResponseEntity.notFound().build();
+    }
+}

e34f8eacab43835ad0494ad62010f217436d8b11
Viktor Dome <v.dome1991@gmail.com>
2021-10-01 11:29:36 +0200

    [update] VenueController routes

--- src/main/java/com/codecool/soundblastr/controller/VenueController.java
+++ src/main/java/com/codecool/soundblastr/controller/VenueController.java
@@ -1,10 +1,13 @@
 package com.codecool.soundblastr.controller;
 
+import com.codecool.soundblastr.controller.exception.IllegalOperationException;
 import com.codecool.soundblastr.entity.*;
 import com.codecool.soundblastr.repository.EventRepository;
 import com.codecool.soundblastr.repository.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,52 +29,50 @@ public class VenueController {
 
     @PostMapping("/new")
     @ResponseBody
-    public Venue addVenue(@RequestBody VenueRequest venueRequest) {
+    public ResponseEntity<Venue> addVenue(@RequestBody VenueRequest venueRequest) {
         Venue venueToAdd = Venue.builder()
-                .imageUrl(venueRequest.getImageUrl())
-                .name(venueRequest.getName())
-                .description(venueRequest.getDescription())
-                .address(venueRequest.getAddress())
-                .capacity(venueRequest.getCapacity())
-                .build();
-        Venue newVenue = venueRepository.save(venueToAdd);
-        newVenue.getAddress().setVenue(newVenue);
-        return newVenue;
+            .imageUrl(venueRequest.getImageUrl())
+            .name(venueRequest.getName())
+            .description(venueRequest.getDescription())
+            .address(venueRequest.getAddress())
+            .capacity(venueRequest.getCapacity())
+            .build();
+
+        Venue vv = venueRepository.save(venueToAdd);
+        return ResponseEntity.ok(vv);
     }
 
     @GetMapping("/{venueId}")
     public Venue getVenue(@PathVariable Long venueId) {
-        return venueRepository.findById(venueId).orElse(null);
+        return venueRepository.findById(venueId).orElseThrow(() ->
+            new DataAccessResourceFailureException("Venue not found!"));
     }
 
     @GetMapping("/all")
-    public List<Venue> getAllVenues() {
-        return venueRepository.findAll();
+    public ResponseEntity<List<Venue>> getAllVenues() {
+        return ResponseEntity.ok(venueRepository.findAll());
     }
 
     @DeleteMapping("/{venueId}")
-    public JsonMessage deleteVenue(@PathVariable Long venueId) {
+    public ResponseEntity<Object> deleteVenue(@PathVariable Long venueId) {
 
         if (eventRepository.findEventsByVenueId(venueId).size() != 0) {
-            return new JsonMessage(Status.NO_ACTION, "Venue #" + venueId + " has associated events and it cannot be deleted.");
+            throw new IllegalOperationException("Venue #" + venueId + " has associated events and it cannot be deleted.");
         }
 
         try {
             venueRepository.deleteById(venueId);
-            return new JsonMessage(Status.OK, "Successfully deleted venue #" + venueId + ".");
-        } catch (EmptyResultDataAccessException e) {
-            return new JsonMessage(Status.NO_ACTION, "Venue #" + venueId + " not found, nothing happened");
+            return ResponseEntity.ok("Successfully deleted venue #" + venueId + ".");
+        } catch (DataAccessException e) {
+            throw new DataAccessResourceFailureException("Venue #" + venueId + " not found!");
         }
     }
 
     @PutMapping("/{venueId}")
     @ResponseBody
-    public Object updateVenue(@PathVariable Long venueId, @RequestBody VenueRequest venueRequest) {
-        Venue venueToUpdate = venueRepository.findById(venueId).orElse(null);
-
-        if (venueToUpdate == null) {
-            return new JsonMessage(Status.NO_ACTION, "Venue #" + venueId + " not found, nothing happened.");
-        }
+    public ResponseEntity<Venue> updateVenue(@PathVariable Long venueId, @RequestBody VenueRequest venueRequest) {
+        Venue venueToUpdate = venueRepository.findById(venueId).orElseThrow(() -> new DataAccessResourceFailureException(
+            "Venue not found!"));
 
         venueToUpdate.setImageUrl(venueRequest.getImageUrl());
         venueToUpdate.setName(venueRequest.getName());
@@ -80,8 +81,8 @@ public class VenueController {
         venueToUpdate.setDescription(venueRequest.getDescription());
 
         Venue newVenue = venueRepository.save(venueToUpdate);
-        newVenue.getAddress().setVenue(newVenue);
-        return newVenue;
+
+        return ResponseEntity.ok(newVenue);
     }
 
 }
